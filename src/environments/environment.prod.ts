4f3b5578c465f8f60c73ed568df318764e63acbb
Lam Huynh <thanhlam0290@gmail.com>
2021-08-14 14:40:52 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEHlYR735wqWd75OErdEYwVyV-gfyMYnc",
+    authDomain: "file-upload-angluar.firebaseapp.com",
+    databaseURL: "https://file-upload-angluar-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "file-upload-angluar",
+    storageBucket: "file-upload-angluar.appspot.com",
+    messagingSenderId: "726218414719",
+    appId: "1:726218414719:web:e73e81fd9df052484f7d4a",
+    measurementId: "G-JXZ8NS87ZG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'xxx',
-    authDomain: 'bezkoder-firebase.firebaseapp.com',
-    databaseURL: 'https://bezkoder-firebase.firebaseio.com',
-    projectId: 'bezkoder-firebase',
-    storageBucket: 'bezkoder-firebase.appspot.com',
-    messagingSenderId: 'xxx',
-    appId: 'xxx'
+    apiKey: "AIzaSyCEHlYR735wqWd75OErdEYwVyV-gfyMYnc",
+    authDomain: "file-upload-angluar.firebaseapp.com",
+    databaseURL: "https://file-upload-angluar-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "file-upload-angluar",
+    storageBucket: "file-upload-angluar.appspot.com",
+    messagingSenderId: "726218414719",
+    appId: "1:726218414719:web:e73e81fd9df052484f7d4a",
+    measurementId: "G-JXZ8NS87ZG"
   }
 };
